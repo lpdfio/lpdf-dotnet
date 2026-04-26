01d84dcb69d2dafda48a23afea77a94fde53e792
Lucian Pacurar <codesensedev@outlook.com>
2026-04-26 01:33:37 -0500

    update workflow

--- test/obj/Debug/net8.0/Lpdf.Tests.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/Lpdf.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lpdf.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd542ba2e3500a8c73399028eab7b8e1b888e226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b15d522ca3d72a287f1a44c6c9c09588b7ee3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lpdf.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lpdf.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
